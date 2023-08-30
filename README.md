84ea38e8b5cb0b54bc3c7d0d61637d2bc149e1bf
cghlee <143438779+cghlee@users.noreply.github.com>
2023-08-30 13:36:05 +0100

    Add album art functionality, fix bugs
    PNG support and file name prompt for album artwork, fix bugs in track ordering and error message, add additional error checks, update README.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ amounts of classical music tracks (and especially opera) in a uniform fashion.
 
 ## Dependencies
 
-FLAC Tagger requires the `mutagen` module, with relevant documentation linked
+FLAC Tagger requires the `Mutagen` module, with relevant documentation linked
 below:
 
 - [Mutagen module](https://pypi.org/project/mutagen/)
@@ -18,11 +18,12 @@ In the TXT file named `titles.txt`, list track titles in order of ascending
 track number, with one track title per line.
 
 *NOTE: Ensure that tracks to be tagged are in the correct order according to
-existing track number metadata, or else the tagging order will be incorrect.*
+existing track number metadata, or the tagging order will be incorrect.*
 
 If you want to embed album artwork into your FLAC files, move the artwork
-file (in JPG format) into the same directory as `README.md`, `titles.txt`,
-and `tagger.py`, and rename the artwork file to `folder.jpg`.
+file (in JPG or PNG format) into the same directory as `README.md`,
+`titles.txt`, and `tagger.py`, and specify the file name of the album artwork
+file when prompted.
 
 Once the above steps are complete, run `tagger.py` and input your desired
 metadata tags. Currently supported tags are for the composer (also used for
--- tagger.py
+++ tagger.py
@@ -19,6 +19,11 @@ with open('titles.txt', 'r') as file:
     titles = [title.strip() for title in titles]
     file.close()
 
+# Terminate program if no track titles have been imported
+if len(titles) == 0:
+    print('No track titles have been entered into tracks.txt')
+    quit()
+
 # Initialise Mutagen music files and append to a list
 for file_name in music_files:
     music_file = os.path.join(music_folder, file_name)
@@ -26,25 +31,34 @@ for file_name in music_files:
 
 # Ensure length of track titles list is equal to that of mutagen files list
 if len(titles) != len(mutagen_files):
-    print('ERROR: There are %s track titles for %s files', (len(titles),
+    print('ERROR: There are %s track titles for %s files' % (len(titles),
                                                             len(mutagen_files)))
     quit()
 
 # Order list of music files by ascending track number
-mutagen_files.sort(key=lambda x: x['tracknumber'])
+mutagen_files.sort(key=lambda x: int(x['tracknumber'][0]))
 
-# Locate album artwork within folder
-for file in os.listdir():
-    if file.startswith('folder'):
-        artwork_file = file
+# Locate album artwork within folder (optional)
+art_input = input('Name of album artwork file excluding file extension: '\
+                  '(optional)\n').lower()
+artwork_file = ''
+if art_input:
+    for file in os.listdir():
+        if file.lower().startswith(art_input):
+            artwork_file = file
+            break
 
 # Instantiate album artwork
 artwork = Picture()
 artwork.type = 3
-artwork.mime = u'image/jpeg'
-with open(artwork_file, 'rb') as art:
-    artwork.data = art.read()
-    art.close()
+if artwork_file:
+    if artwork_file.endswith('jpg') or artwork_file.endswith('jpeg'):   
+        artwork.mime = u'image/jpeg'
+    elif artwork_file.endswith('png'):
+        artwork.mime = u'image/png'
+    with open(artwork_file, 'rb') as art:
+        artwork.data = art.read()
+        art.close()
 
 # Delete and replace all metadata of music files
 track_number = 1
@@ -59,6 +73,7 @@ for file in mutagen_files:
     file['date'] = [year]
     file['genre'] = [genre]
     file['tracknumber'] = [f'{track_number:02d}']
-    file.add_picture(artwork)
+    if art_input:
+        file.add_picture(artwork)
     track_number += 1
     file.save()
